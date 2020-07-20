664d795ee9981d4cfb240de46ad879cae6c3286a
Waverley Leung <waverley.nyc@gmail.com>
2020-07-20 21:14:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,6 +5,7 @@ def find_min_in_nested_arrays(src)
   row_index = 0
   while row_index < src.count do
     element_index = 0
+    smallest_number = 
     while element_index < src[row_index].count do
-      if src[row_index][element_index].length > 
+      if src[row_index][element_index].length <  
 end
